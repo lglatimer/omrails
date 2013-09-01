41850a86f5af0387de9faec0287c3e5be1139c0c
Laurence Latimer <lgl78.online@gmail.com>
2013-09-01 01:52:29 -0400

    Added Devise forf user sessions

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,6 @@ Omrails::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # In production, :host should be set to the actual host of the application.
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'http://intense-waters-3108.herokuapp.com' }
 
 end
